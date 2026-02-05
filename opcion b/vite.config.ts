199dd1c140df4ec75af26ee4f229f312a29527d6
gechelingabriel-sys <gechelingabriel@gmail.com>
2026-02-05 12:47:47 -0300

    Update vite.config.ts

--- opcion b/vite.config.ts
+++ opcion b/vite.config.ts
@@ -1,28 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/", // en Vercel va en la raíz
+  base: "/",
 });
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-
-  export default defineConfig({
-  base: '/',
-  // ...
-})
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
