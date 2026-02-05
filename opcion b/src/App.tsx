c8b1d29c0d5faa12d5c8562035e928ac80365c59
gechelingabriel-sys <gechelingabriel@gmail.com>
2026-02-05 14:28:30 -0300

    Update App.tsx

--- opcion b/src/App.tsx
+++ opcion b/src/App.tsx
@@ -1,7 +1,5 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import { TooltipProvider } from "@/components/ui/tooltip"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -16,9 +14,8 @@ const basename = import.meta.env.BASE_URL.endsWith("/")
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter basename={basename}>
+
+      <Toaster richColors position="top-right" />      <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
